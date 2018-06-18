4011eb48cbbdf113beae7364211a81530d1408cf
Ryan Wang <ryanwang@Ryans-MacBook-Pro-3.local>
2018-06-18 14:57:21 -0700

    framerwork

---
+++ gypsie/lib/framework.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new RealWorldApp());
+
+class RealWorldApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new RealWorldState();
+  }
+ }
+
+ class RealWorldState extends State<RealWorldApp> {
+   @override
+     Widget build(BuildContext context) {
+       // TODO: implement build
+       return null;
+     }
+ }
