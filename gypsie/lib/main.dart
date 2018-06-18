cdd6a337934637d01573b488874d0f1851eca0e1
Rohan Pandey <rohan@Rohans-MacBook-Pro.local>
2018-06-18 16:59:16 -0700

    hnng

--- gypsie/lib/main.dart
+++ gypsie/lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:vibrate/vibrate.dart' as vib;
 import 'package:location/location.dart';
+import 'dart:convert' as parse;
 
 // input origin (prefill current location)
 // input destination
@@ -13,11 +13,13 @@ var link = "https://maps.googleapis.com/maps/api/directions/json?origin=" + orig
   + "&key=AIzaSyD0zb7Xdwk0FH-TOVbYkCTjSQargAwjb84";
 // Directions API request > save JSON
 var json = http.get(link);
-// print(json);
+var e = parse.jsonDecode(json);
+var steps = e.routes.legs[0].steps;
+var lat = steps[0].end_location.lat;
+var lng = steps[0].end_location.lng;
 
 var cur = (new Location()).getLocation;
-var e = vib.Vibrate.vibrate();
-
+var oof = vib.Vibrate.vibrate();
 
 // loop:
 // get current location 
@@ -25,5 +27,5 @@ var e = vib.Vibrate.vibrate();
 // calculate distance to next waypoint
 // if distance < 1000ft send vibration
 while (/* cur != destination */) {
-  if (cur )
+  if (cur > dest - 200)
 }
